d3902578c620ddcab967de2204af1846a028bcc5
Visnu Pitiyanuvath <visnupx@gmail.com>
2011-03-04 17:57:09 -0800

    change the name to something useful

--- lib/rack-process-name.rb
+++ lib/rack-process-name.rb
@@ -2,12 +2,14 @@ module Rack
   class ProcessName
     def initialize(app)
       @app = app
+      @name = $0
     end
 
     def call(env)
       req = Rack::Request.new(env)
-      $0 = req.inspect
+      $0 = "#{@name}: #{req.request_method} #{req.url}"
       @app.call(env)
+      $0 = @name
     end
   end
 end
